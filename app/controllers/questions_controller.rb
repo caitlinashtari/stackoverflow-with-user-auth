c5078255373ed3d043897ed83c08469b17243f3d
caitlin ashtari and haley swain <haley@>
2017-02-14 13:58:07 -0800

    add functionality for creating new questions under a specific user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,22 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = current_user.questions.new
+  end
+
+  def create
+    @question = current_user.questions.new(question_params)
+    if @question.save
+      redirect_to user_questions_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+private
+  def question_params
+    params.require(:question).permit(:name, :description)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214192951) do
+ActiveRecord::Schema.define(version: 20170214194527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170214192951) do
     t.string   "password_salt"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "name"
   end
 
 end
